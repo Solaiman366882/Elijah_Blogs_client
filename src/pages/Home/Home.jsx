37805e62d5ea47ba82ad71f48a6599257fa52bd0
Md.Solaiman <md.solaiman366882@gmail.com>
2023-11-05 16:49:01 +0600

    Important files and folder uploaded for project

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+
+const Home = () => {
+    return (
+        <div>
+            this is home.
+            <Button></Button>
+        </div>
+    );
+};
+
+export default Home;
